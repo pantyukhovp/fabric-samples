d99923dac40b9387f4b8856d1a7fc595aa3b9573
Павел Пантюхов <pantyukhov.p@gmail.com>
2017-12-02 19:30:13 +0300

    add clinics

--- chaincode/fabcar/fabcar.go
+++ chaincode/fabcar/fabcar.go
@@ -38,16 +38,25 @@ import (
 	sc "github.com/hyperledger/fabric/protos/peer"
 )
 
-// Define the Smart Contract structure
 type SmartContract struct {
 }
 
-// Define the car structure, with 4 properties.  Structure tags are used by encoding/json library
 type User struct {
-	FirstName   string `json:"firstName"`
+	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-	ImageUrl string `json:"imageUrl"`
-	Hash string `json:"hash"`
+	ImageUrl  string `json:"imageUrl"`
+	Hash      string `json:"hash"`
+}
+
+type Clinic struct {
+	Name string `json:"name"`
+}
+
+type Research struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	From   string `json:"from"`
+	To     string `json:"to"`
 }
 
 /*
@@ -66,6 +75,9 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := APIstub.GetFunctionAndParameters()
+
+	//queryAllClinics
+
 	// Route to the appropriate handler function to interact with the ledger appropriately
 	if function == "queryPersons" {
 		return s.queryCar(APIstub, args)
@@ -73,13 +85,15 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 		return s.initLedger(APIstub)
 	} else if function == "createCar" {
 		return s.createCar(APIstub, args)
-	} else if function == "queryAllCars" {
+	} else if function == "queryPersons" {
 		return s.queryAllUsers(APIstub)
 	} else if function == "changeCarOwner" {
 		return s.changeCarOwner(APIstub, args)
+	} else if function == "queryAllClinics" {
+		return s.queryAllClinics(APIstub)
 	}
 
-	return shim.Error("Invalid Smart Contract function name.")
+	return shim.Error("Invalid Smart Contract function name. 1")
 }
 
 func (s *SmartContract) queryCar(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
@@ -95,6 +109,7 @@ func (s *SmartContract) queryCar(APIstub shim.ChaincodeStubInterface, args []str
 func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
 	users := []User{
 		User{FirstName: "Pavel", LastName: "Pantyukhov", ImageUrl: "https://pp.userapi.com/c638918/v638918847/3d1d9/s_auB5cvB6M.jpg", Hash: "3u891738291hdiawhduiawdhiuawd"},
+		User{FirstName: "Pavel", LastName: "Pantyukhov", ImageUrl: "https://pp.userapi.com/c638918/v638918847/3d1d9/s_auB5cvB6M.jpg", Hash: "3u891738291hdiawhduiawdhiuawd"},
 		User{FirstName: "Maksim", LastName: "Kuznetsov", ImageUrl: "https://pp.userapi.com/c307310/v307310903/602d/Gyr1qLrB23Q.jpg", Hash: "2903821390218390jdioawjdiowajdoiaw"},
 		User{FirstName: "Yakov", LastName: "Kanner", ImageUrl: "", Hash: "1892737128djwaiodjiawodjwoi"},
 		User{FirstName: "Vitaliy", LastName: "Melnik", ImageUrl: "", Hash: "1231jdlawmdklawjdklawnmdlkwandjakwn"},
@@ -108,6 +123,42 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Respo
 		APIstub.PutState("USER"+strconv.Itoa(i), userAsBytes)
 		fmt.Println("Added", users[i])
 		i = i + 1
+	}
+
+	// Add clinic
+
+	clinics := []Clinic{
+		Clinic{Name: "НИИ онкологии им. Н.Н. Петрова"},
+		Clinic{Name: "Университетская Клиника"},
+		Clinic{Name: "Городской Клинический Онкологический Диспансер"},
+		Clinic{Name: "Медлайн-Сервис на Октябрьском поле"},
+		Clinic{Name: "Он клиник на Новом Арбате"},
+		Clinic{Name: "Центр эндохирургии и литотрипсии (ЦЭЛТ)"},
+		Clinic{Name: "Клиника Столица на Ленинском, 90"},
+		Clinic{Name: "Клиника Столица на Арбате"},
+		Clinic{Name: "Европейский медицинский центр на ул. Щепкина"},
+		Clinic{Name: "ЭлЭн"},
+		Clinic{Name: "Ортодонт комплекс"},
+		Clinic{Name: "Simpladent на Дмитровской"},
+		Clinic{Name: "Simpladent на Пролетарской"},
+		Clinic{Name: "Перинатальный медицинский центр Мать и Дитя"},
+		Clinic{Name: "Медлайн-Сервис на Полежаевской"},
+		Clinic{Name: "Медлайн-Сервис на Сходненской"},
+		Clinic{Name: "Медлайн-Сервис на Октябрьском поле"},
+		Clinic{Name: "Медлайн-Сервис на ВДНХ"},
+	}
+
+	// for i := 0; i < 10; i++ {
+	// 	append(clinics, Clinic{Name: "Simpladent на Пролетарской"})
+	// }
+
+	j := 0
+	for j < len(clinics) {
+		fmt.Println("j is ", j)
+		clinicAsBytes, _ := json.Marshal(clinics[j])
+		APIstub.PutState("CLINIC"+strconv.Itoa(j), clinicAsBytes)
+		fmt.Println("Added", clinics[j])
+		j = j + 1
 	}
 
 	return shim.Success(nil)
@@ -125,6 +176,49 @@ func (s *SmartContract) createCar(APIstub shim.ChaincodeStubInterface, args []st
 	APIstub.PutState(args[0], userAsBytes)
 
 	return shim.Success(nil)
+}
+
+func (s *SmartContract) queryAllClinics(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+	startKey := "CLINIC0"
+	endKey := "CLINIC20"
+
+	resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- queryAllCars:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
 }
 
 func (s *SmartContract) queryAllUsers(APIstub shim.ChaincodeStubInterface) sc.Response {
